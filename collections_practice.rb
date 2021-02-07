a5b9f233910d04d81b72b15d7e70651e2640274d
Bryanjazo <bryanjazo88@gmail.com>
2021-02-06 19:37:36 -0800

    commit tree 7bfa1cb391fd97afebf809eebdde10882012b709

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+require 'pry'
 
+def sort_array_asc(array)
+array.sort 
+end
+
+def sort_array_desc(array)
+array.sort.reverse
+end
+
+def sort_array_char_count(count)
+count.sort_by{|name| name.length}
+end
+
+def swap_elements(ar)
+ar[1], ar[2] = ar[2], ar[1]
+ar
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+array.each do |item|
+    item[2] = "$"
+end
+end
+
+def find_a(array)
+   array.select do |pick|
+    pick[0] == "a"
+   end
+end
+
+def sum_array(array)
+    #inding.pry
+array.inject{|sum, x| sum + x}
+end
+
+def add_s(array)
+array.collect do |word|
+    if array[1] == word
+        word
+    else
+        word + "s"
+    end
+    end
+end
